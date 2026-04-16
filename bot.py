46b037e1c3772f100fea27edb82f26ea214e42eb
sherhonkhidirov-gif <sherhonkhidirov@gmail.com>
2026-04-17 04:45:19 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.enums import ParseMode
 
 # --- SOZLAMALAR ---
-# Eski xato qatorni o'chirib, buni yozing:
 API_TOKEN = "8642617336:AAEtQc8o0YEqKRH7Rt8vedsP9G08dv4p0FY"
 
 ADMIN_ID = 8642617336 
